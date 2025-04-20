ecc6b6e564551ef08a63a69761ef7793ae305731
Siva774 <manikantaman2@gmail.com>
2025-04-20 16:02:52 +0530

    Updated App and index files, removed unused components and assets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-// eslint-disable-next-line
-import React from 'react';
-import AddTask from './components/AddTask';
-import TaskList from './components/TaskList';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-const App = () => {
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState("");
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (taskName.trim()) {
+      const newTask = {name: taskName, completed: false };
+      setTasks([...tasks, newTask]);
+      setTaskName(""); 
+    }
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const toggleTaskCompletion = (id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+  };
+
   return (
-    <div className="App">
-      <h1>To-Do List</h1>
-      <AddTask />
-      <TaskList />
+    <div className="app">
+      <h1 className="heading">Task Tracker</h1>
+    
+    <div className="text-and-taskbutton">
+      <form onSubmit={addTask}>
+        <input
+          type="text"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          placeholder="Enter new task" className="input-box"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      </div>
+
+     
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            className={`task-item ${task.completed ? "completed" : ""}`}
+            onClick={() => toggleTaskCompletion(task.id)}
+          >
+            <span>{task.name}</span>
+            <button onClick={(e) => { e.stopPropagation(); deleteTask(task.id); }}><i class="fa fa-trash"></i></button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
